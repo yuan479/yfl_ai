989f7ab624732505d16c2361fb35d396c710dfc4
yuanfelin <3516955776@qq.com>
2025-07-23 11:02:38 +0800

    登录鉴权jsonwebtoken

--- react/jwt-demo/README.md
+++ react/jwt-demo/README.md
@@ -1,3 +1,24 @@
 # jwt 登录鉴权
 - isLogin .user 全局状态 zustand
 - mock 登录模拟
+  - apifox api请求模拟，不用写页面，就可以发送请求
+    相当于curl 
+
+    -server 种下一个cookie 唯一sid 值 sid =>user
+    - 每次请求中 从cookie 读取sid
+    - 服务器端就知道是我们了
+
+- 登录和用户鉴权方案 JWT (Json Web Token)
+  - {id：112，username：‘帅得惊动党中央’，level:4} 用户json格式的数据
+  - 它会通过一种算法，生成一个 hash 串，成为令牌，不会重复
+  - token 由服务器端给的令牌
+  - 每次请求带上token 
+  - decode 解码，得到上面的用户信息
+
+- jsonwebtoken
+  jwt 鉴权的库 它提供两个方法
+  sign 颁发token  user
+  verify 验证token  user
+  - pnpm i jwt
+  - import jwt from "jsonwebtoken";
+  - sign 
--- react/jwt-demo/mock/login.js
+++ react/jwt-demo/mock/login.js
@@ -1,3 +1,9 @@
+import jwt from "jsonwebtoken";
+const {sign} = jwt;
+
+
+const secret = 'O0OOoo0O0ooO0O0o0' // 密钥  安全性 编码的时候加密，解码时解密
+
 // login 模块 mock 
 export default [
     {
@@ -7,7 +13,26 @@ export default [
         response: (req, res) => {
             // req, username, password
             const {username, password} = req.body;
+            if(username!=='admin' ||password!=='123456'){
+                return {
+                    code:1,
+                    messgae:'用户名或密码错误'
+                }
+            }
+            // 接下来生成token 颁发令牌
+            //json 用户数据，把它编码到token里面去
+            const token =jwt.sign({
+                user:{
+                    id:"001",
+                    username:"admin",
+                }
+            },secret,{
+                expiresIn:3600
+            })
+            console.log(token,"<--------token")
+
             return {
+                token,
                 username,
                 password
             }
