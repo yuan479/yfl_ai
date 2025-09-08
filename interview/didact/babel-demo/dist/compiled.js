af49b83088cb419c0577d146145d191b20278b2c
selencestar <3516955776@qq.com>
2025-09-08 20:05:07 +0800

    Didact

---
+++ interview/didact/babel-demo/dist/compiled.js
@@ -0,0 +1,5 @@
+/** @jsx Didact.createElement*/
+
+const element = Didact.createElement("div", {
+  id: "app"
+}, Didact.createElement("a", null, "a1"), Didact.createElement("a", null, "a2"));
---
+++ interview/didact/babel-demo/src/index.jsx
@@ -0,0 +1,9 @@
+/** @jsx Didact.createElement*/
+
+const element = (
+    <div id="app">
+        <a>a1</a>
+        <a>a2</a>
+
+    </div>
+)
