6ac8b69628e6dcda0ac7ad309f526598eb500f05
selencestar <3516955776@qq.com>
2025-09-11 13:28:41 +0800

    supabase数据库

--- interview/node/supabase/demo/lib/supabaseClient.mjs
+++ interview/node/supabase/demo/lib/supabaseClient.mjs
@@ -2,6 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { config } from 'dotenv'
 
+config()
 
 export const supabase = createClient(
     process.env.SUPABASE_URL,
--- interview/node/supabase/demo/main.mjs
+++ interview/node/supabase/demo/main.mjs
@@ -1,10 +1,11 @@
 import { supabase } from './lib/supabaseClient.mjs'
 //Backend as service
 // 异步，node
-const {data,error} = await supabase.from("todos").insert({
+/* const {error} = await supabase.from("todos").insert({
     title:'AI 从入门到入土',
-    iscomplate:false
+    is_complete:false
 })
-config()
 
-console.log(error);
+console.log(error); */
+const {data,error} = await supabase.from("todos").select("*")
+console.log(data);
