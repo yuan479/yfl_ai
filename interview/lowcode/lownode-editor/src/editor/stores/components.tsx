0eef86648b7ad4ac98c6e8839810ebb9e7d1e1e9
selencestar <3516955776@qq.com>
2025-09-19 10:47:51 +0800

    低代码编辑器 - 参数问题修复

--- interview/lowcode/lownode-editor/src/editor/stores/components.tsx
+++ interview/lowcode/lownode-editor/src/editor/stores/components.tsx
@@ -26,7 +26,7 @@ export const useComponentsStore = create<State & Actions>(
             components: [
                 {
                     id: 0,
-                    name: 'Page',
+                    name: '???',
                     props: {},
                     children: [],
                 }
